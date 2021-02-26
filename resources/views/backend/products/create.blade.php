a00d76892cc12d5abbfe02e43acd167404853f28
Sirbu-Anca <ana.sirbu@5psolutions.eu>
2021-02-26 13:27:12 +0200

    #16 Product http://pm.5psolutions.net/public/index.php?path_info=projects/training-ana-sirbu/tasks/16 display edit form and create form

---
+++ resources/views/backend/products/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="col-md-3">
+        <form action="{{ route('backend.products.store') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-3">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title"
+                       placeholder="{{__('Title')}}" value="{{ old('title')}}">
+                @error('title')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control @error('description') is-invalid @enderror" name="description"
+                       placeholder="{{__('Description')}}" value="{{ old('description') }}">
+                @error('description')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <input type="number" class="form-control @error('price') is-invalid @enderror" name="price"
+                       placeholder="{{__('Price')}}" value="{{ old('price') }}">
+                @error('price')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <input type="file" class="form-control @error('image_name') is-invalid @enderror" name="image_name"
+                       placeholder="{{__('Image_name')}}">
+                @error('image_name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+            <div>
+                <button type="submit" name="submit">{{__('Save')}}</button>
+            </div>
+        </form>
+    </div>
+@endsection
