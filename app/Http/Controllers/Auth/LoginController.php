b31f6c002535f5f249b193730af06c61d4ec7d23
Sirbu-Anca <ana.sirbu@5psolutions.eu>
2021-03-02 10:24:08 +0200

    #14 Login http://pm.5psolutions.net/public/index.php?path_info=projects/training-ana-sirbu/tasks/14 redirect to index page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -39,14 +40,18 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    protected function authenticated(Request $request, $user)
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    protected function authenticated(Request $request)
     {
         if ($request->input('email') === config('auth.admin.email')) {
             return redirect()
                 ->route('backend.products.index');
         } else {
             return redirect()
-                ->route('home_page');
+                ->route('products.index');
         }
     }
 }
