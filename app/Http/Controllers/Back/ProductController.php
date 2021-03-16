39c4b5a97e8fbfa65f9cb355e12482bd62a99da6
Sirbu-Anca <ana.sirbu@5psolutions.eu>
2021-03-16 10:44:37 +0200

    #22 Products http://pm.5psolutions.net/public/index.php?path_info=projects/training-ana-sirbu/tasks/22

--- app/Http/Controllers/Back/ProductController.php
+++ app/Http/Controllers/Back/ProductController.php
@@ -4,12 +4,9 @@ namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
-use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
